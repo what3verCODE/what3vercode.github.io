a8c8123be4a9e0e5a0ecf9b0ff4bdaa18d5bd824
Alexandr Nokhrin <32194395+what3verCODE@users.noreply.github.com>
2020-08-02 23:26:42 +0500

    Add files via upload

---
+++ employees-list/precache-manifest.7cf1a573e8acae3e020fd8b957efc88e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f08f432cee38d37ee0d20e035ad26a9",
+    "url": "/employees-list/index.html"
+  },
+  {
+    "revision": "58b541e583a01dd8f4bd",
+    "url": "/employees-list/static/css/main.b8b4ad79.chunk.css"
+  },
+  {
+    "revision": "824779ecba635a1675f5",
+    "url": "/employees-list/static/js/2.105e9347.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/employees-list/static/js/2.105e9347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58b541e583a01dd8f4bd",
+    "url": "/employees-list/static/js/main.6c0c7991.chunk.js"
+  },
+  {
+    "revision": "3f122856da40463b59aa",
+    "url": "/employees-list/static/js/runtime-main.ff9e448a.js"
+  }
+]);
